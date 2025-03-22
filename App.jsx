5f9dda1131ac508e83a481095896830ceda0a571
Galsha97 <shavivgal@gmail.com>
2025-03-22 11:51:49 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+
+import IBITDashboard from './IBITDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <IBITDashboard />
+    </div>
+  );
+}
+
+export default App;
